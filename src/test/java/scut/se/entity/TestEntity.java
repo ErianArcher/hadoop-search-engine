3a83d0caaba875a5824df144adcde1aaaae99407
Liangjp <jungg1996@gmail.com>
2019-06-20 11:28:57 +0800

    新增了空构造器和equals方法

--- src/test/java/scut/se/entity/TestEntity.java
+++ src/test/java/scut/se/entity/TestEntity.java
@@ -1,8 +1,13 @@
 package scut.se.entity;
 
+import java.util.Objects;
+
 public class TestEntity {
     private String testCol1;
     private String testCol2;
+
+    public TestEntity() {
+    }
 
     public TestEntity(String testCol1, String testCol2) {
         this.testCol1 = testCol1;
@@ -23,5 +28,19 @@ public class TestEntity {
 
     public void setTestCol2(String testCol2) {
         this.testCol2 = testCol2;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        TestEntity that = (TestEntity) o;
+        return Objects.equals(testCol1, that.testCol1) &&
+                Objects.equals(testCol2, that.testCol2);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(testCol1, testCol2);
     }
 }
